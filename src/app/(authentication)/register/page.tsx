ddc505ea2cc66129ffc3b2e820efe3ac5513016f
Sjors Eveleens <sjorspieter@live.nl>
2023-05-26 09:12:06 +0200

    refactor: register form as client component

--- src/app/(authentication)/register/page.tsx
+++ src/app/(authentication)/register/page.tsx
@@ -1,44 +1,11 @@
-"use client";
+import RegisterForm from "@/components/RegisterForm";
 
 export default function Page() {
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    console.log(Object.fromEntries(formData));
-  }
-
   return (
-    <form onSubmit={(event) => handleSubmit(event)}>
-      <label>name</label>
-      <input
-        name="name"
-        type="text"
-        defaultValue="John Doe"
-      />
+    <main>
+      <h1>Register</h1>
 
-      <label>Email</label>
-      <input
-        name="email"
-        type="email"
-        defaultValue="johndoe@example.com"
-      />
-
-      <label>Password</label>
-      <input
-        name="password"
-        type="password"
-        defaultValue="password"
-      />
-
-      <label>Password confirmation</label>
-      <input
-        name="password_confirmation"
-        type="password"
-        defaultValue="password"
-      />
-
-      <button type="submit">Sign in</button>
-    </form>
+      <RegisterForm />
+    </main>
   );
 }
