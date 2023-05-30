52c30db7b9290f8400cbf648dcf4cd5bd0998311
Sjors Eveleens <sjorspieter@live.nl>
2023-05-30 19:39:45 +0200

    docs: remove comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,7 +22,6 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
