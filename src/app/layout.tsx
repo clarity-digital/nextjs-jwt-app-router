4d8b8607e6cc46ce3caf79d1d3841603f76ef501
Sjors Eveleens <sjorspieter@live.nl>
2023-06-05 08:20:43 +0200

    fix: prevent server component error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }: Props) {
     <html lang="en">
       <AuthProvider>
         <body>
+          {/* @ts-expect-error Server Component */}
           <AppNavigation />
 
           {children}
